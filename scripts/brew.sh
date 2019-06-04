39066ca74179988945a3e09b0daf527c7235179d
Akshith Gunasekaran <akshith.573@gmail.com>
2019-06-04 03:27:00 -0700

    brew

---
+++ scripts/brew.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# install brew
+brew_setup(){
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update && brew upgrade && brew cleanup
+}
+
+install_basic(){
+    echo "installing basic ..."
+    brew install \
+        git \
+        git-lfs \
+        tree \
+        vim \
+        wget
+}
+
+install_dev(){
+    echo "installing dev ..."
+    brew install \
+        go \
+        hugo \
+        python
+
+    brew cask install \
+        docker \
+        visual-studio-code
+}
+
+install_writing(){
+    echo "installing writing ..."
+    brew cask install \
+        mactex \
+        mathpix-snipping-tool \
+        trezor-bridge
+}
+
+install_extras(){
+    echo "installing extras ..."
+    brew cask install \
+        lumen \
+        trezor-bridge
+}
+
+install_all(){
+    echo "installing all ..."
+    install_basic
+    install_dev
+    install_writing
+    install_extras
+}
